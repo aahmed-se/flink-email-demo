6ec479c63469d424086fb6df9eb736800eb82f13
Ali Ahmed <a.ahmed@criteo.com>
2017-04-04 23:10:56 -0700

    Update
    Signed-off-by: Ali Ahmed <a.ahmed@criteo.com>

--- src/main/java/enron/email/Driver.java
+++ src/main/java/enron/email/Driver.java
@@ -1,80 +1,28 @@
 package enron.email;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.aggregation.Aggregations;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
-import javax.mail.Address;
-import javax.mail.internet.MimeMessage;
-
 import java.io.File;
 import java.nio.file.Files;
-import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Driver {
 
-    public static class MailSplitter implements FlatMapFunction<MimeMessage, Tuple2<String, Integer>> {
-
-        @Override
-        public void flatMap(MimeMessage value, Collector<Tuple2<String, Integer>> out) throws Exception {
-
-            for(Address address : value.getAllRecipients()){
-                out.collect(new Tuple2<String, Integer>(
-                        address.toString() + ":" + Utility.DATE_FORMAT.parse(value.getSentDate().toString()).toString(),
-                        1));
-            }
-        }
-    }
-
-//    public class WC {
-//        public String word;
-//        public int count;
-//        // [...]
-//    }
-//
-//    // ReduceFunction that sums Integer attributes of a POJO
-//    public class WordCounter implements ReduceFunction<WC> {
-//        @Override
-//        public WC reduce(WC in1, WC in2) {
-//            return new WC(in1.word, in1.count + in2.count);
-//        }
-//    }
-
     public static void main(String[] args) throws Exception {
+
         String logPath = Utility.emailsPath;
-        List<MimeMessage> messages = new ArrayList<>();
+
+        List<String> messages = new ArrayList<>();
         for(File entry : Utility.listFilesForFolder(new File(logPath))){
             String content = new String(Files.readAllBytes(entry.toPath()));
-            MimeMessage message = Utility.stringToMimeMessage(content);
-            messages.add(message);
-            message.getSentDate();
-            //System.out.println(message.getSubject() + " " + message.getFrom().length);
+            //MimeMessage message = Utility.stringToMimeMessage(content);
+            //System.out.println(content);
+            messages.add(content);
         }
 
-        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        DataSet<MimeMessage> messageDataSet = env.fromCollection(messages);
-
-        System.out.println(messageDataSet.count());
-
-        DataSet<Tuple2<String, Integer>> counts =
-                // split up the lines in pairs (2-tuples) containing: (word,1)
-                messageDataSet.flatMap(new MailSplitter())
-                        .groupBy(0)
-                        .aggregate(Aggregations.SUM, 1);
-
-        // emit result
-        counts.print();
-
-        // execute program
-        env.execute("WordCount Example");
-
+        //new EmailsCountByDay().getMailCountByDay(messages);
+        new DirectMessage().topDirectMessageReceiver(messages);
+        new BroadCastMessage().topBroadcastMessageSender(messages);
     }
 
 }
--- src/main/java/enron/email/Utility.java
+++ src/main/java/enron/email/Utility.java
@@ -7,7 +7,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
@@ -18,9 +17,8 @@ import java.util.stream.Collectors;
 public class Utility {
 
     final static String emailsPath = "/Users/a.ahmed/workspace/enron-email/src/main/resources/enron_with_categories/";
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
-    //final static String emailsPath = "/enron_with_categories/*/*.txt";
+    public static final SimpleDateFormat DATE_FORMAT_DAY = new SimpleDateFormat("yyyy-MM-dd");
 
     public static MimeMessage stringToMimeMessage(String content) {
         Session s = Session.getDefaultInstance(new Properties());
@@ -33,18 +31,6 @@ public class Utility {
         }
     }
 
-    public static String getFirstOfFrom(MimeMessage msg) {
-        try {
-            if (msg != null && msg.getFrom() != null && msg.getFrom().length > 0) {
-                return msg.getFrom()[0].toString();
-            }
-        } catch (MessagingException e) {
-            System.err.println("ERROR reading msg" + msg.toString());
-            return null;
-        }
-        return null;
-    }
-
     public static List<File> listFilesForFolder(final File folder) throws IOException {
         return Files.walk(folder.toPath())
                 .filter(Files::isRegularFile)
@@ -54,36 +40,4 @@ public class Utility {
 
     }
 
-    // We use this to group all messages between A,B
-    static class UnorderedUserPair implements Serializable {
-        final String user1;
-        final String user2;
-        final int hashcode;
-
-        UnorderedUserPair(String in1, String in2) {
-            if (in1.compareTo(in2) < 0) {
-                user1 = in1;
-                user2 = in2;
-            } else {
-                user1 = in2;
-                user2 = in1;
-            }
-
-            hashcode = (user1 + user2).hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj != null && (obj instanceof UnorderedUserPair)) {
-                UnorderedUserPair other = (UnorderedUserPair) obj;
-                return other.user1.equals(user1) && other.user2.equals(user2);
-            }
-            return false;
-        }
-
-        @Override
-        public int hashCode() {
-            return hashcode;
-        }
-    }
 }
